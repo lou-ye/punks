2df530b18fe4fef6874a5feb8bd4017a210eb28f
apple zona oeste <applezonaoeste@MacBook-Air-de-apple.local>
2022-05-08 21:48:53 -0300

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const deploy = async () => {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contract with the account:", deployer.address);
+  
+    const Punks = await ethers.getContractFactory("Punks");
+    const deployed = await Punks.deploy();
+  
+    console.log("Punks is deployed at:", deployed.address);
+  };
+  
+  deploy()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.log(error);
+      process.exit(1);
+    });
